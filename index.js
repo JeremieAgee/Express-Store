1c22e2b6025f4c9885a4f9fe91dfc67636af803f
JeremieAgee <you@example.com>
2024-08-29 15:08:12 -0500

    Added more comments

--- index.js
+++ index.js
@@ -13,20 +13,28 @@ const supabase = require("./supabaseInstance");
 // create an express application
 const app = express();
 
+/*Import Util Classes
+These classes help maintain project structure.
+They also store all the data from the database without a constant connection
+*/
 const Store = require("./utils/Store");
 const Beverage = require("./utils/Beverage");
 const Snack = require("./utils/Snack");
+// Intitalize the shop class
 const onlineShop = new Store(`Jeremie's Store`);
 
-// define a port
-const PORT = 4000;
+//Create function to set the store with data in the db before server initialization
 async function setStore() {
   try {
+
+    //Get snacks table from db and set the store's snacks.
     const snacks = await supabase.get("/snacks");
     const newSnacks = snacks.data.map((snack) => {
       return new Snack(snack.id, snack.name, snack.description, snack.price, snack.catagory, snack.inStock, snack.count)
     })
     onlineShop.setSnacks(newSnacks);
+
+    //Get beverages table from db and set the store's beverages.
     const beverages = await supabase.get("/beverages");
     const newBeverages = beverages.data.map((beverage) => {
       return new Beverage(beverage.id, beverage.name, beverage.description, beverage.price, beverage.catagory, beverage.inStock, beverage.count)
@@ -40,6 +48,9 @@ async function setStore() {
 
 // Call the function to set store.
 setStore();
+
+// define a port
+const PORT = 4000;
 
 // Define our Middleware
 // Use CORS Middleware
@@ -57,7 +68,11 @@ app.get("/", (req, res, next) => {
 // Route to Get all Snacks
 app.get("/snacks", async (req, res, next) => {
   try {
-    res.json(onlineShop.snacks);
+
+    /*Note: Instead of making a supabase request for the snacks table 
+    each time we return the set snacks from the store.
+    */
+   res.json(onlineShop.snacks);
   } catch (err) {
     next(err);
   }
@@ -85,11 +100,15 @@ app.post("/snacks", (req, res, next) => {
         .status(400)
         .json({ message: "Missing required fields!!" });
     } else {
+      //Create snack obj to input into the store and supabase;
       const newSnack = new Snack(onlineShop.snacks.length + 1, name, description, price, catagory, inStock, count)
       supabase.post("/snacks", newSnack);
       onlineShop.snacks.push(newSnack)
     }
-    // send a response of the added data
+    /* send a response of the added data.
+    Note: Data is sent from the store. 
+    This shows the central store on the server is updated with the db
+    */
     res.status(201).json(onlineStore.snacks[onlineStore.snacks.length]);
   } catch (err) {
     next(err);
@@ -99,6 +118,7 @@ app.post("/snacks", (req, res, next) => {
 // Route to update a snack
 app.put("/snacks/:id", (req, res, next) => {
   try {
+
     // destructure our request.body object so we can store the fields in variables
     const { name, description, price, catagory, inStock, count } = req.body;
     const thisId = req.params.id;
@@ -122,6 +142,8 @@ app.put("/snacks/:id", (req, res, next) => {
 //Route to remove Snack
 app.delete("/snacks/:id", (req, res, next) => {
   try {
+
+    //We send a delete request to supabase then remove the snack item from the store
     const thisId = req.params.id;
     supabase.delete("/snacks?id=eq." + thisId);
     res.json({ message: `The snack with ${thisId}` });
@@ -146,7 +168,7 @@ app.use((error, request, response, next) => {
 app.use((request, response, next) => {
   response.status(404).json({
     error:
-      "Resource not found. Are you sure you're looking in the right place?",
+      "Resource not found. Try again or try different spot",
   });
 });
 
