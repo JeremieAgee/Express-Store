bbcbe42fd9961c4a2b9a08245fb48f92f8266cea
JeremieAgee <you@example.com>
2024-09-16 20:29:13 -0500

    added line to insure that NODE_ENV is set for test

--- api/tests/api.test.js
+++ api/tests/api.test.js
@@ -1,10 +1,13 @@
 // Import Dotenv
 require("dotenv").config();
+process.env.NODE_ENV = 'development';
 const request = require("supertest");
 const { app, server } = require("../index");
 
+
 describe("Snacks API", () => {
   // Test GET all snacks
+ 
   it("should return all snacks", async () => {
     const response = await request(app)
       .get("/snacks")
@@ -15,6 +18,7 @@ describe("Snacks API", () => {
   //!name || !description || !price || !catagory || !inStock || !count
   // Test POST a new snack
   it("should add a new snack", async () => {
+
     const newSnack = {
       name: "Potato Chips",
       description: "Crispy and salty chips",
@@ -35,6 +39,7 @@ describe("Snacks API", () => {
   });
   // Test GET a single snack by ID
   it("should return a snack by ID", async () => {
+
     const snackId = 1;
     const response = await request(app)
       .get(`/snacks/${snackId}`)
@@ -46,6 +51,7 @@ describe("Snacks API", () => {
 
   // Test PUT to update a snack by ID
   it("should update a snack by ID", async () => {
+
     const snackId = 1; // Use a valid ID
     const updatedSnack = {
       name: "BBQ Chips",
@@ -67,6 +73,7 @@ describe("Snacks API", () => {
 
   // Test DELETE a snack by ID
   it("should delete a snack by ID", async () => {
+
     const snackId = 0; // Change this ID for testing purposes
     const response = await request(app)
       .delete(`/snacks/${snackId}`)
